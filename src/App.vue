7070667a23e289d01bc71d82af94492853371fde
GiacomoDiNardo <vest_95@hotmail.it>
2022-06-20 17:55:01 +0200

    add bonus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <PageTitle></PageTitle>
+    <Hero></Hero>
   </div>
 </template>
 
 <script>
 import PageTitle from "./components/PageTitle.vue";
+import Hero from "./components/HeroJumbo.vue"
 
 export default {
   components: {
     PageTitle,
+    Hero,
   }
 }
 </script>
---
+++ src/components/HeroJumbo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="jumbotron">
+        <h1>Questo è il Bonus</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.jumbotron {
+    background-image: url("https://picsum.photos/500/500");
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 500px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
